76105ca6b9be91e70b3c4555013c32e653f84c6a
Juan Manuel Pardo Garcia <plexus.jpardo@iberostar.com>
2025-11-18 23:35:27 +0100

    I've performed clean code refactoring on several classes. The ErrorResult class has been moved (namespace changed), and some of the other classes have been modified as well.

---
+++ sources/EBOS.Core/Primitives/ErrorResult.cs
@@ -0,0 +1,10 @@
+﻿using EBOS.Core.Primitives.Interfaces;
+
+namespace EBOS.Core.Primitives;
+
+public class ErrorResult(string message, int code, string? exceptionMsg) : IErrorResult
+{
+    public string? ExceptionMsg { get; set; } = exceptionMsg;
+    public int Code { get; set; } = code;
+    public string Message { get; set; } = message;
+}
