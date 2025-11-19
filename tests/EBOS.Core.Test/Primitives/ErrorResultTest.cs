52c68ea4e574a15d7d984c728ec7558ce4103552
jpardogarcia7310 <jmpardogarcia7310@icloud.com>
2025-11-19 13:52:12 +0100

    Update ErrorResultTest.cs

--- tests/EBOS.Core.Test/Primitives/ErrorResultTest.cs
+++ tests/EBOS.Core.Test/Primitives/ErrorResultTest.cs
@@ -4,36 +4,39 @@ using EBOS.Core.Primitives.Interfaces;
 namespace EBOS.Core.Test.Primitives;
 
 public class ErrorResultTests
-{
-    #region Interface contract
-    [Fact]
-    public void ErrorResult_ImplementsIErrorResult()
     {
-        var error = new ErrorResult("msg", 1, "ex");
+        #region Interface contract
 
-        Assert.IsType<IErrorResult>(error, exactMatch: false);
-    }
-    #endregion
+        [Fact]
+        public void ErrorResult_ImplementsIErrorResult()
+        {
+            var error = new ErrorResult("msg", 1, "ex");
 
-    #region Constructor behavior
-    [Fact]
-    public void Constructor_SetsAllProperties()
-    {
-        var error = new ErrorResult("An error occurred", 100, "Exception details");
+            Assert.IsAssignableFrom<IErrorResult>(error);
+        }
 
-        Assert.Equal("An error occurred", error.Message);
-        Assert.Equal(100, error.Code);
-        Assert.Equal("Exception details", error.ExceptionMsg);
-    }
+        #endregion
 
-    [Fact]
-    public void Constructor_AllowsNullExceptionMsg()
-    {
-        var error = new ErrorResult("Error without exception", 200, null);
+        #region Constructor behavior
 
-        Assert.Equal("Error without exception", error.Message);
-        Assert.Equal(200, error.Code);
-        Assert.Null(error.ExceptionMsg);
+        [Fact]
+        public void Constructor_SetsAllProperties()
+        {
+            var error = new ErrorResult("An error occurred", 100, "Exception details");
+
+            Assert.Equal("An error occurred", error.Message);
+            Assert.Equal(100, error.Code);
+            Assert.Equal("Exception details", error.ExceptionMsg);
+        }
+
+        [Fact]
+        public void Constructor_AllowsNullExceptionMsg()
+        {
+            var error = new ErrorResult("Error without exception", 200, null);
+
+            Assert.Equal("Error without exception", error.Message);
+            Assert.Equal(200, error.Code);
+            Assert.Null(error.ExceptionMsg);
     }
 
     [Fact]
@@ -60,22 +63,24 @@ public class ErrorResultTests
     public void Constructor_AllowsNullMessageAtRuntime()
     {
         string? message = null;
+
         var error = new ErrorResult(message!, 10, "ex");
 
         Assert.Null(error.Message);
         Assert.Equal(10, error.Code);
         Assert.Equal("ex", error.ExceptionMsg);
     }
+
     #endregion
 
     #region Property setters
+
     [Fact]
     public void Message_PropertyIsSettable()
     {
-        var error = new ErrorResult("initial", 1, null)
-        {
-            Message = "updated"
-        };
+        var error = new ErrorResult("initial", 1, null);
+
+        error.Message = "updated";
 
         Assert.Equal("updated", error.Message);
     }
@@ -83,10 +88,9 @@ public class ErrorResultTests
     [Fact]
     public void Code_PropertyIsSettable()
     {
-        var error = new ErrorResult("msg", 1, null)
-        {
-            Code = 999
-        };
+        var error = new ErrorResult("msg", 1, null);
+
+        error.Code = 999;
 
         Assert.Equal(999, error.Code);
     }
@@ -94,10 +98,9 @@ public class ErrorResultTests
     [Fact]
     public void ExceptionMsg_PropertyIsSettableToNonNull()
     {
-        var error = new ErrorResult("msg", 1, null)
-        {
-            ExceptionMsg = "new exception details"
-        };
+        var error = new ErrorResult("msg", 1, null);
+
+        error.ExceptionMsg = "new exception details";
 
         Assert.Equal("new exception details", error.ExceptionMsg);
     }
@@ -105,29 +108,30 @@ public class ErrorResultTests
     [Fact]
     public void ExceptionMsg_PropertyIsSettableToNull()
     {
-        var error = new ErrorResult("msg", 1, "ex")
-        {
-            ExceptionMsg = null
-        };
+        var error = new ErrorResult("msg", 1, "ex");
+
+        error.ExceptionMsg = null;
 
         Assert.Null(error.ExceptionMsg);
     }
+
     #endregion
 
     #region Combined scenarios
+
     [Fact]
     public void Properties_CanBeMutatedIndependently()
     {
-        var error = new ErrorResult("initial", 100, "ex1")
-        {
-            Message = "changed",
-            Code = 200,
-            ExceptionMsg = "ex2"
-        };
+        var error = new ErrorResult("initial", 100, "ex1");
+
+        error.Message = "changed";
+        error.Code = 200;
+        error.ExceptionMsg = "ex2";
 
         Assert.Equal("changed", error.Message);
         Assert.Equal(200, error.Code);
         Assert.Equal("ex2", error.ExceptionMsg);
     }
+
     #endregion
 }
